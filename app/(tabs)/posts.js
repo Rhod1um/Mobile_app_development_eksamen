ebabfbf638050a988d9c2e4d39a6ae20302805c1
Fluttendut <ded_19@hotmail.com>
2023-10-20 00:10:55 +0200

    Stuck..

--- app/(tabs)/posts.js
+++ app/(tabs)/posts.js
@@ -1,8 +1,35 @@
 import { Stack, useRouter } from "expo-router";
 import { StyleSheet, Text, View, Button, Platform } from "react-native";
+import { useState } from "react";
+import { log } from "react-native-reanimated";
 
 export default function Posts() {
-    const router = useRouter();
+    const [posts, setPosts] = useState([]);
+    const router = useRouter(); 
+
+    const API_URL = "https://expo-modal-tab-nav-default-rtdb.firebaseio.com/";
+
+    
+    useEffect(() => {
+      async function getPosts() {
+        const response = await fetch(`${API_URL}/posts.json`); // Fetch posts data from the specified API endpoint
+        const dataObj = await response.json(); // Parse the response data into an object
+
+        // Convert the data object into an array of posts with 'id' properties
+        const postsArray = Object.keys(dataObj).map((key) => ({ id: key, ...dataObj[key] })); // from object to array
+
+        // Sort the posts in descending order based on their 'createdAt' property
+        postsArray.sort((postA, postB) => postB.createdAt - postA.createdAt); // sort by timestamp/ createdBy
+
+        setPosts(postsArray); // Set the sorted posts in your application's state
+      }
+    }, []);
+
+    
+
+    
+
+
 
     function showCreateModal() {
         router.push("/create");
@@ -27,6 +54,8 @@ export default function Posts() {
         </View>
     );
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
